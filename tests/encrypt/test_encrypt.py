19849e18d0e3ee63cd6eb32623c7022483be00d8
Guilherme Garcia <garciaguig@gmail.com>
2023-04-25 17:01:58 -0300

    test: Add tests for `encrypt_message`

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,28 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    even_expected = "noht_yP"
+    odd_expected = "tyP_noh"
+    reversed_expected = "nohtyP"
+
+    # Even key
+    even_result = encrypt_message("Python", 2)
+    assert even_expected == even_result
+
+    # Odd key
+    odd_result = encrypt_message("Python", 3)
+    assert odd_expected == odd_result
+
+    # Out of range key
+    reversed_result = encrypt_message("Python", 10)
+    assert reversed_expected == reversed_result
+
+    # Invalid message
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(123, 2)
+
+    # Invalid key
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("Python", "invalid")
