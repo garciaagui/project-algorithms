f90864b1b7eb8ba4cf24b331b1d713bb57727109
Guilherme Garcia <garciaguig@gmail.com>
2023-04-25 16:15:47 -0300

    feat: Implements `study_schedule` function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui..."""
-    raise NotImplementedError
+    counter = 0
+
+    if type(target_time) != int:
+        return None
+
+    for entry, exit in permanence_period:
+        if type(entry) != int or type(exit) != int:
+            return None
+
+        if target_time >= entry and target_time <= exit:
+            counter += 1
+
+    return counter
