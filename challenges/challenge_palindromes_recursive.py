b988c940ea669b27f59e8b1ab950d1332efbab5c
Guilherme Garcia <garciaguig@gmail.com>
2023-04-26 10:25:20 -0300

    feat: Implements `is_palindrome_recursive` function

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    word = word.lower()
+
+    if not len(word) or word[low_index] != word[high_index]:
+        return False
+
+    if low_index == (len(word) - 1) and high_index == 0:
+        return word[low_index] == word[high_index]
+
+    else:
+        low_index += 1
+        high_index -= 1
+        return is_palindrome_recursive(word, low_index, high_index)
