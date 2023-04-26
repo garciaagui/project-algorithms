16666e76ad2ae5e4d3690e8402a48301ec6a6a9d
Guilherme Garcia <garciaguig@gmail.com>
2023-04-26 14:54:08 -0300

    feat: Implements `is_palindrome_iterative` function

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,16 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not len(word):
+        return False
+
+    word = word.lower()
+    low_index = 0
+    high_index = len(word) - 1
+
+    while low_index < (len(word) - 1) and high_index > 0:
+        if word[low_index] != word[high_index]:
+            return False
+
+        low_index += 1
+        high_index -= 1
+
+    return True
